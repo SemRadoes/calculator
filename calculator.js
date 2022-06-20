df9ff2e73aec7b2782369b217168927b4b39a4d8
Aws3m <semradijs@hotmail.com>
2022-06-20 13:30:56 +0200

    finished temp visuals

---
+++ calculator.js
@@ -0,0 +1,50 @@
+function add1(){
+    document.querySelector('#1')
+}
+function add2(){
+    document.querySelector('#2')
+}
+function add3(){
+    document.querySelector('#3')
+}
+function add4(){
+    document.querySelector('#4')
+}
+function add5(){
+    document.querySelector('#5')
+}
+function add6(){
+    document.querySelector('#6')
+}
+function add7(){
+    document.querySelector('#7')
+}
+function add8(){
+    document.querySelector('#8')
+}
+function add9(){
+    document.querySelector('#9')
+}
+function add0(){
+    document.querySelector('#0')
+}
+function CE(){
+    document.querySelector('#CE')
+}
+function addPlus(){
+    document.querySelector('#plus')
+}
+function addMin(){
+    document.querySelector('#min')
+}
+function addPoint(){
+    document.querySelector('#punt')
+}
+function addKomma(){
+    document.querySelector('#komma')
+}
+
+
+function calculate(){
+    
+}
